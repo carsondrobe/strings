ddbf9f5b2d955db22ae3825d93876851451cde8a
carsondrobe <carsondrobe@gmail.com>
2024-03-20 11:58:17 -0700

    Add form validation for registration fields

--- register.php
+++ register.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 session_start();
+$usernameErr = $emailErr = $dobErr = $passwordErr = $retypePasswordErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn = mysqli_connect("localhost", "root", "", "strings");
@@ -11,37 +12,84 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("ERROR: Could not connect. " . mysqli_connect_error());
     }
 
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-
-    // Check for unique User
-    $unique_user = "SELECT * FROM User WHERE username = '$username' or email = '$email'";
-    $result = mysqli_query($conn, $unique_user);
-
-    if (mysqli_num_rows($result) > 0) {
-        echo "<script> alert('Username or Email already exists!') </script>";
-        exit();
+    if (empty($_POST['username'])) {
+        $usernameErr = "Username is required";
+    } else {
+        $username = test_input($_POST['username']);
+        if (!preg_match('/^[A-Za-z][A-Za-z0-9]{5,31}$/', $username)) {
+            $usernameErr = "Username can only contain letters, numbers, and underscores";
+        }
     }
 
-    $dob = $_POST['dob'];
-    $password = $_POST['password'];
-
-    // TODO: Add profile picture upload
-    $profilePicture = null;
-
-    $sql = "INSERT INTO User (username, password, email, dob) VALUES ('$username', '$password', '$email', '$dob')";
-
-    $result = mysqli_query($conn, $sql);
-
-    if ($result) {
-        // Send them to the login page
-        header("Location: login.php");
-        exit();
+    if (empty($_POST['email'])) {
+        $emailErr = "Email is required";
     } else {
-        echo "Registration failed.";
+        $email = test_input($_POST['email']);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+        }
+    }
+
+    if (empty($_POST['dob'])) {
+        $dobErr = "Date of Birth is required";
+    } else {
+        // Validate the format of the date
+        $dob = test_input($_POST['dob']);
+        if (preg_match("/^\d{4}-\d{2}-\d{2}$/", $dob) == 0) {
+            $dobErr = "Invalid date format. Please use YYYY-MM-DD";
+        } else {
+            // Check if the date is in the past (not future DOB)
+            $currentDate = date("Y-m-d");
+            if ($dob >= $currentDate) {
+                $dobErr = "Date of Birth must be in the past";
+            }
+        }
+    }
+
+    if (empty($_POST['password'])) {
+        $passwordErr = "Password is required";
+    } else {
+        $password = test_input($_POST['password']);
+        if ($password != $_POST['retype-pass']) {
+            $retypePasswordErr = "Passwords do not match";
+        }
+    }
+
+    if (empty($usernameErr) && empty($emailErr) && empty($dobErr) && empty($passwordErr) && empty($retypePasswordErr)) {
+        // Check for unique User
+        $unique_user = "SELECT * FROM User WHERE username = '$username' or email = '$email'";
+        $result = mysqli_query($conn, $unique_user);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "<script> alert('Username or Email already exists!') </script>";
+            exit();
+        }
+
+        // TODO: Add profile picture upload
+        $profilePicture = null;
+
+        $sql = "INSERT INTO User (username, password, email, dob) VALUES ('$username', '$password', '$email', '$dob')";
+
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            // Send them to the login page
+            header("Location: login.php");
+            exit();
+        } else {
+            echo "Registration failed.";
+        }
     }
 
     mysqli_close($conn);
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 ?>
 
@@ -75,24 +123,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="form-group">
                     <label for="inputUsername1">Username</label>
                     <input type="text" class="form-control" id="inputUsername1" name="username" placeholder="Enter username" required>
+                    <span class="error"><?php echo $usernameErr; ?></span>
                 </div>
                 <div class="form-group">
                     <label for="inputEmail1">Email</label>
                     <input type="email" class="form-control" id="inputEmail1" name="email" aria-describedby="emailHelp" placeholder="Enter email" required>
+                    <span class="error"><?php echo $emailErr; ?></span>
                     <small id="emailHelp" class="form-text text-muted">Don't worry, your email is safe with us
                         :)</small>
                 </div>
                 <div class="form-group">
                     <label for="inputDOB1">Date of Birth</label>
                     <input type="date" class="form-control" id="inputDOB1" name="dob" required>
+                    <span class="error"><?php echo $dobErr; ?></span>
                 </div>
                 <div class="form-group">
                     <label for="inputPassword1">Password</label>
                     <input type="password" class="form-control" id="inputPassword1" name="password" placeholder="Password" required>
+                    <span class="error"><?php echo $passwordErr; ?></span>
                 </div>
                 <div class="form-group">
                     <label for="inputPassword1">Retype Password</label>
-                    <input type="password" class="form-control" id="retypePassword1" placeholder="Retype password" required>
+                    <input type="password" class="form-control" id="retypePassword1" placeholder="Retype password" name="retype-pass" required>
+                    <span class="error"><?php echo $retypePasswordErr; ?></span>
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary" name="submit">Register!</button>
