a3b78099450ed1d5ea6dafea551eb4998c3c3bd5
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-20 15:17:06 -0700

    Previous commit v4.

--- index.php
+++ index.php
@@ -222,12 +222,12 @@ try {
                         <a href="view_post.php?discussionID='.$row['discussionID'].'" class="post-link">                        
                             <h4 class="card-title">'.($row['title']).'</h4>
                             <img src="data:image/jpeg;base64,'.$imageData.'" class="card-img-top" alt="Discussion Image" id="discussion-image">
-                            <p class="card-text"></p>
+                            <p class="card-text">'.($row['content']).'</p>
                         </a>
-                        <div class="d-flex justify-content-end mt-3">
-                            <button type="button" class="btn btn-outline-success me-2">+ ('.($row['upvotes']).')</button>
-                            <button type="button" class="btn btn-outline-danger">- ('.($row['downvotes']).')</button>
-                        </div>
+                        // <div class="d-flex justify-content-end mt-3">
+                        //     <button type="button" class="btn btn-outline-success me-2">+ ('.($row['upvotes']).')</button>
+                        //     <button type="button" class="btn btn-outline-danger">- ('.($row['downvotes']).')</button>
+                        // </div>
                     </div>
                 </div>
             </div>
