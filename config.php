1dec55bea4f63a831ee8508d36a8a1881e6c4785
carsondrobe <carsondrobe@gmail.com>
2024-03-20 15:14:15 -0700

    config

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 // Path: config.php
 //Server Configuration
-// $host_name = "localhost";
-// $db_user = "90172180";
-// $db_pass = "90172180";
-// $db_name = "db_90172180";
+$host_name = "localhost";
+$db_user = "90172180";
+$db_pass = "90172180";
+$db_name = "db_90172180";
 
 // Personal Connection
-$host_name = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "strings";
+// $host_name = "localhost";
+// $db_user = "root";
+// $db_pass = "";
+// $db_name = "strings";
 
 $conn = mysqli_connect("$host_name", "$db_user", "$db_pass", "$db_name");
 
