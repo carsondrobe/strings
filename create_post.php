c4017dc07c1185c2f22750e10c43a4ed6073b651
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-21 13:33:27 -0700

    Fixed issue where image not uploading correctly.

--- create_post.php
+++ create_post.php
@@ -11,6 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $time_posted = date('Y-m-d');
     $upvotes = 0;
     $downvotes = 0;
+
     // Check if content is over limit
     if (strlen($content) > 5000) {
         $_SESSION['error_message'] = "Your post content exceeds the maximum allowed length of 5000 characters. Please shorten your post.";
@@ -18,6 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
+   $imageData = NULL;
     // If image upload
     if (isset($_FILES['postImage']) && $_FILES['postImage']['error'] == 0) {
         // Check if file is over limit
@@ -26,26 +28,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             header("Location: index.php");
             exit();
         }
-        $image = file_get_contents($_FILES['postImage']['tmp_name']);
-        $discussion_picture = base64_encode($image);
-
-    // If no image uploaded
-    } else {
-        $discussion_picture = '';
+        $imageData = file_get_contents($_FILES['postImage']['tmp_name']);
     }
-
+    // Create prepared statement and bind parameters
     $stmt = $conn->prepare("INSERT INTO Discussions (username, title, content, discussion_picture, time_posted, category, upvotes, downvotes) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssssii", $username, $title, $content, $discussion_picture, $time_posted, $category, $upvotes, $downvotes);
-
+    $stmt->bind_param("ssssssii", $username, $title, $content, $null, $time_posted, $category, $upvotes, $downvotes);
+    if ($imageData !== NULL) {
+        $stmt->send_long_data(3, $imageData); 
+    }
+    // Execute prepared statement
     if ($stmt->execute()) {
-        echo "Post created successfully!";
         header("Location: index.php");
         exit();
     } else {
         echo "Error: " . $stmt->error;
     }
+    // Close the prepared statement
     $stmt->close();
-    // $conn->close();
 } else {
     echo "Invalid request.";
 }
