c32b683ccde734107c36892cd1899ced9aa86add
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-21 12:39:16 -0700

    Replaced headbar with back to top on view post page.

--- view_post.php
+++ view_post.php
@@ -32,6 +32,17 @@
             </div>
         </div>
     </div> -->
+    <!-- <script>
+    window.addEventListener('scroll', function () {
+        var headbar = document.getElementById('headbar');
+
+        if (window.scrollY > headbar.offsetTop) {
+            headbar.classList.add('fixed-top');
+        } else {
+            headbar.classList.remove('fixed-top');
+        }
+    });
+</script> -->
 
     <!-- PHP script for displaying a post on the home page -->
     <?php 
@@ -159,23 +170,47 @@
         }
     </script>
 
+    <!-- Back to top button -->
+    <button type="button" class="btn btn-outline-black" id="btn-back-to-top">
+        Back to Top
+    </button>
+
+    <!-- Back to Top Logic -->
+    <script>
+        //Get the button
+        let mybutton = document.getElementById("btn-back-to-top");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction();
+        };
+
+        function scrollFunction() {
+            if (
+                document.body.scrollTop > 500 ||
+                document.documentElement.scrollTop > 500
+            ) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+        // When the user clicks on the button, scroll to the top of the document
+        mybutton.addEventListener("click", backToTop);
+
+        function backToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
+
     <!-- BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
     <!-- BOOTSTRAP -->
 
-    <script>
-        window.addEventListener('scroll', function () {
-            var headbar = document.getElementById('headbar');
 
-            if (window.scrollY > headbar.offsetTop) {
-                headbar.classList.add('fixed-top');
-            } else {
-                headbar.classList.remove('fixed-top');
-            }
-        });
-    </script>
 </body>
 
 </html>
