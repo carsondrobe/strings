600984c5859408ef0289f9d3f46f96ff2a31d3bd
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-21 14:59:53 -0700

    Fixed syntax errors.

--- view_post.php
+++ view_post.php
@@ -11,152 +11,138 @@
     <!-- BOOTSTRAP -->
     <link href="css/view_post.css" rel="stylesheet">
     <link href="css/navbar.css" rel="stylesheet">
-
-
     </head>
 
 <body>
 
-<?php include 'navbar.php'; ?>
+    <?php include 'navbar.php'; ?>
 
-<!-- PHP script for displaying a post on the home page -->
-<?php 
-session_start();
-include 'config.php';
-try {
-    if(isset($_GET['discussionID'])) {
-        $discussionId = $_GET['discussionID'];
-        $query = "SELECT * FROM Discussions WHERE discussionID = " . $discussionId;
-        $result = $conn->query($query);
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $imageData = base64_encode($row['discussion_picture']);
-            echo '
-                <div class="container justify-content-center" id="headbar">
-                    <div class="row align-items-center">
-                        <div class="col " id="headtext">
-                            <button type="button" class="btn btn-outline-success me-2">+</button>
-                            <button type="button" class="btn btn-outline-danger">-</button>
-                        </div>
-                        <div class="col-6 " id="headtext">'.($row['title']).'</div>
-                        <div class="col text-end white-text">
-                            <a href="index.php" class="btn-close white-button">&nbsp;&nbsp;&nbsp;&nbsp;</a>
+    <!-- PHP script for displaying a post on the home page -->
+    <?php 
+    session_start();
+    include 'config.php';
+    try {
+        if(isset($_GET['discussionID'])) {
+            $discussionId = $_GET['discussionID'];
+            $query = "SELECT * FROM Discussions WHERE discussionID = " . $discussionId;
+            $result = $conn->query($query);
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $imageData = base64_encode($row['discussion_picture']);
+                echo '
+                    <div class="container justify-content-center" id="headbar">
+                        <div class="row align-items-center">
+                            <div class="col " id="headtext">
+                                <button type="button" class="btn btn-outline-success me-2">+</button>
+                                <button type="button" class="btn btn-outline-danger">-</button>
+                            </div>
+                            <div class="col-6 " id="headtext">'.($row['title']).'</div>
+                            <div class="col text-end white-text">
+                                <a href="index.php" class="btn-close white-button">&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="container justify-content-center">
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <p class="card-text"><strong>Posted by: ✏️ </strong>'.($row['username']).' | <strong>Published on:</strong>
-                                    '.($row['time_posted']).'</p>
-                                    <h4 class="card-title">'.($row['title']).'</h4>
-                                    <img src="data:image/jpeg;base64,'.$imageData.'" class="card-img-top img-fluid mx-auto d-block"
-                                        style="max-width: 400px; margin-bottom: .25em;" alt="Discussion Image">
-                                    <p class="card-text">'.($row['content']).'</p>
-                                    <hr>
-                                    <div class="d-flex justify-content-end mt-3">
-                                        <button type="button" class="btn btn-outline-success me-2">+ ('.($row['upvotes']).')</button>
-                                        <button type="button" class="btn btn-outline-danger">- ('.($row['downvotes']).')</button>
-                                    </div>
-                                    <p class="card-text">
-                                        3 Comments
-                                    </p>';
-            if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
-                echo '
-                                            <div class="card mb-3">
-                                                <div class="card-body">
-                                                    <h5 class="card-title">Create a Comment</h5>
-                                                    <form>
-                                                        <div class="mb-3">
-                                                            <textarea class="form-control" id="commentContent" rows="3" required></textarea>
-                                                        </div>
-                                                        <button class="btn btn-outline-info" type="submit">
-                                                            Submit
-                                                        </button>
-                                                    </form>
-                                                </div>
-                                            </div>
-                ';
-            }
-            echo '
-                                </div>
-                            </div>
-                            <br>
-            ';
-            // Dynamically generate comments here
-            $query2 = "SELECT * FROM Comments WHERE discussionID = " . $discussionId;
-            $result2 = $conn->query($query2);
-            if($result2->num_rows > 0) {
-                $comment = $result2->fetch_assoc();
-                if ($comment['replyingTo'] == NULL) {
-                    $reply = false;
-                } else (
-                    $replyingTo = $comment['replyingTo'];
-                )
-                echo '
-                            <div class="card" id="comment">
-                                <div class="card-body">
-                                    <p class="card-text">Written By: '.$comment['username'].' | '.$comment['timePosted'].'</p>
-                ';
-                if($reply != false) {
+                    <div class="container justify-content-center">
+                        <div class="row justify-content-center">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <p class="card-text"><strong>Posted by: ✏️ </strong>'.($row['username']).' | <strong>Published on:</strong>
+                                        '.($row['time_posted']).'</p>
+                                        <h4 class="card-title">'.($row['title']).'</h4>
+                                        <img src="data:image/jpeg;base64,'.$imageData.'" class="card-img-top img-fluid mx-auto d-block"
+                                            style="max-width: 400px; margin-bottom: .25em;" alt="Discussion Image">
+                                        <p class="card-text">'.($row['content']).'</p>
+                                        <hr>
+                                        <div class="d-flex justify-content-end mt-3">
+                                            <button type="button" class="btn btn-outline-success me-2">+ ('.($row['upvotes']).')</button>
+                                            <button type="button" class="btn btn-outline-danger">- ('.($row['downvotes']).')</button>
+                                        </div>
+                                        <p class="card-text">
+                                            3 Comments
+                                        </p>';
+                if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) {
                     echo '
-                                    <p class="card-text">Responding To: '.$comment['replyingTo'].'</p>
+                                                <div class="card mb-3">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Create a Comment</h5>
+                                                        <form>
+                                                            <div class="mb-3">
+                                                                <textarea class="form-control" id="commentContent" rows="3" required></textarea>
+                                                            </div>
+                                                            <button class="btn btn-outline-info" type="submit">
+                                                                Submit
+                                                            </button>
+                                                        </form>
+                                                    </div>
+                                                </div>
                     ';
                 }
                 echo '
-                                    <p class="card-text">'.$comment['content'].'</p>
+                                    </div>
                                 </div>
-                            </div>
+                                <br>
                 ';
-            }
-                            // <div class="card" id="first-comment">
-                            //     <div class="card-body">
-                            //         <p class="card-text">✏️ iPhoneGuy | 1 day ago</p>
-                            //         <p class="card-text">This is the best news ever!</p>
-                            //     </div>
-                            // </div>
-                            // <div class="card" id="second-comment">
-                            //     <div class="card-body">
-                            //         <p class="card-text">✏️ AndroidNerd | 1 day ago</p>
-                            //         <p class="card-text">I diagree you fool.</p>
-                            //     </div>
-                            // </div>
-                            // <div class="card" id="first-comment">
-                            //     <div class="card-body">
-                            //         <p class="card-text">✏️ UselessJoe | 1 day ago</p>
-                            //         <p class="card-text">I am so confused! Iaculis at erat pellentesque adipiscing commodo elit at
-                            //             imperdiet dui. Semper
-                            //             feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Odio ut sem nulla pharetra diam
-                            //             sit amet nisl suscipit. Neque viverra justo nec ultrices dui sapien eget mi proin. Dignissim
-                            //             enim sit amet venenatis urna cursus eget nunc scelerisque. Tortor consequat id porta nibh
-                            //             venenatis. Interdum varius sit amet mattis vulputate enim nulla. Adipiscing at in tellus
-                            //             integer feugiat.</p>
-                            //     </div>
-                            // </div>
-            echo '
+                // Dynamically generate comments here
+                $query2 = "SELECT * FROM Comments WHERE discussionID = " . $discussionId;
+                $result2 = $conn->query($query2);
+                if ($result2->num_rows > 0) {
+                    while ($comment = $result2->fetch_assoc()) {
+                        echo '
+                            <div class="card" id="comment">
+                                <div class="card-body">
+                                    <p class="card-text">Written By: ' . $comment['username'] . ' | ' . $comment['timePosted'] . '</p>';
+                                    
+                        if ($comment['replyingTo'] != NULL) {
+                            echo '<p class="card-text">Responding To: ' . $comment['replyingTo'] . '</p>';
+                        }
+                        
+                        echo '<p class="card-text">' . $comment['content'] . '</p>
+                                </div>
+                            </div>';
+                    }
+                }
+                                // <div class="card" id="first-comment">
+                                //     <div class="card-body">
+                                //         <p class="card-text">✏️ iPhoneGuy | 1 day ago</p>
+                                //         <p class="card-text">This is the best news ever!</p>
+                                //     </div>
+                                // </div>
+                                // <div class="card" id="second-comment">
+                                //     <div class="card-body">
+                                //         <p class="card-text">✏️ AndroidNerd | 1 day ago</p>
+                                //         <p class="card-text">I diagree you fool.</p>
+                                //     </div>
+                                // </div>
+                                // <div class="card" id="first-comment">
+                                //     <div class="card-body">
+                                //         <p class="card-text">✏️ UselessJoe | 1 day ago</p>
+                                //         <p class="card-text">I am so confused! Iaculis at erat pellentesque adipiscing commodo elit at
+                                //             imperdiet dui. Semper
+                                //             feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Odio ut sem nulla pharetra diam
+                                //             sit amet nisl suscipit. Neque viverra justo nec ultrices dui sapien eget mi proin. Dignissim
+                                //             enim sit amet venenatis urna cursus eget nunc scelerisque. Tortor consequat id porta nibh
+                                //             venenatis. Interdum varius sit amet mattis vulputate enim nulla. Adipiscing at in tellus
+                                //             integer feugiat.</p>
+                                //     </div>
+                                // </div>
+                echo '
+                            </div>
                         </div>
                     </div>
-                </div>
-            ';
+                ';
 
+            } else {
+                echo "No post found with the provided discussionID.";
+            }
         } else {
-            echo "No post found with the provided discussionID.";
+            echo "Could not find a discussionID.";
         }
-    } else {
-        echo "Could not find a discussionID.";
+    // $conn->close();
+    } catch(Exception $e) {
+        die($e->getMessage());
     }
-// $conn->close();
-} catch(Exception $e) {
-    die($e->getMessage());
-}
-?>
-
-
-
-
-
+    ?>
     <!-- BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
