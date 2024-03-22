8db0ab4c18398fd228a9bb7ebc013a701f30d280
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-21 19:16:59 -0700

    pc

--- view_post.php
+++ view_post.php
@@ -169,12 +169,9 @@
     <!-- JavaScript functions for editing comments -->
     <script>
         function editComment(commentID) {
-            commentID = commentID.toString();
             document.getElementById('edit-form-' + commentID).style.display = "block";
         }
         function cancelEdit(commentID) {
-            commentID = commentID.toString();
-            document.getElementById('comment-content-' + commentID).style.display = "none";
             document.getElementById('edit-form-' + commentID).style = "none";
         }
     </script>
