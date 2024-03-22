93280002db9caf4f93d4a48da10a24bbcf3ae2bb
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-21 20:16:59 -0700

    Trying to get edit post modal to open.

--- view_post.php
+++ view_post.php
@@ -137,11 +137,13 @@
                         ';
                         if($_SESSION['username'] == $comment['username']) {
                             echo '
-                                                            <button onclick="editComment('.$comment['commentID'].')" class="btn btn-outline-info btn-sm style="text-align: left; display: inline;" id="edit-comment-btn">Edit Comment</button>
-                                                            <form method="post"  action="delete_comment.php" style="text-align: right;">
-                                                                <input type="hidden" name="commentID" value='.$comment['commentID'].'>
-                                                                <button type="submit" class="btn btn-danger btn-sm" style="text-align: right; display: inline;" id="delete-comment-btn" onclick="return confirm(\'Are you sure you want to delete this comment?\');">Delete Comment</button>
-                                                            </form>
+                                        <a class="nav-link active" type="button" aria-disabled="true" data-bs-toggle="modal" data-bs-target="#editPostModal">
+                                            <button onclick="editComment('.$comment['commentID'].')" class="btn btn-outline-info btn-sm style="text-align: left; display: inline;" id="edit-comment-btn">Edit Comment</button>
+                                        </a>                            
+                                        <form method="post"  action="delete_comment.php" style="text-align: right;">
+                                            <input type="hidden" name="commentID" value='.$comment['commentID'].'>
+                                            <button type="submit" class="btn btn-danger btn-sm" style="text-align: right; display: inline;" id="delete-comment-btn" onclick="return confirm(\'Are you sure you want to delete this comment?\');">Delete Comment</button>
+                                        </form>
                             ';
                         }
                         echo '
@@ -199,10 +201,6 @@
             document.getElementById('editPostTitle').value = title;
             document.getElementById('editPostContent').value = content;
             document.getElementById('editPostCategory').value = category;
-            var editModal = new bootstrap.Modal(document.getElementById('editPostModal'), {
-            keyboard: false
-            });
-            editModal.show();
         }
     </script>
     <!-- BOOTSTRAP -->
