a70ebfd7805c08c71f9a590d61e37ae65b58585e
carsondrobe <carsondrobe@gmail.com>
2024-03-22 22:46:54 -0700

    improve

--- search_users.php
+++ search_users.php
@@ -17,13 +17,17 @@ if (isset($_GET['query'])) {
     $result = $stmt->get_result();
 
 
-    $response = '';
+    if ($result->num_rows > 0) {
+        $response = '';
 
-    while ($row = $result->fetch_assoc()) {
-        $response .= '<li class="list-group-item d-flex justify-content-between align-items-center" style="margin: 1em;">';
-        $response .= '✏️ ' . $row['username'];
-        $response .= '<button type="button" class="btn btn-danger" onclick="deleteUser(' . $row['userID'] . ', this.parentNode)">Delete</button>';
-        $response .= '</li>';
+        while ($row = $result->fetch_assoc()) {
+            $response .= '<li class="list-group-item d-flex justify-content-between align-items-center" style="margin: 1em;">';
+            $response .= '✏️ ' . $row['username'];
+            $response .= '<button type="button" class="btn btn-danger" onclick="deleteUser(' . $row['userID'] . ', this.parentNode)">Delete</button>';
+            $response .= '</li>';
+        }
+    } else {
+        $response = '<h2>No users match that search result</h2>';
     }
 
     echo $response;
