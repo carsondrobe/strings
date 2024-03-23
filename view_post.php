b9ad04c36872aa6b622c7abbbfddf5023d3c6cc6
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-22 21:47:48 -0700

    Troubleshoot upvote and downvote buttons.

--- view_post.php
+++ view_post.php
@@ -238,8 +238,8 @@
         function ratingSubmitted() {
             // document.getElementById('upvote-btn').style.display = "none";
             // document.getElementById('downvote-btn').style.display = "none";
-            document.getElementById('upvote-btn').classList.add = "disabled";
-            document.getElementById('downvote-btn').classList.add = "disabled";
+            document.getElementById('upvote-btn').classList.add("disabled");
+            document.getElementById('downvote-btn').classList.add("disabled");
         }
 
     </script>
