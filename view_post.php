0fe07c78cc8b3f62b7f85720d8c7fe2ee97cf398
abijeet-dhillon <abijeetdhillon@gmail.com>
2024-03-23 12:23:57 -0700

    Trying to reset character count upon submission.

--- view_post.php
+++ view_post.php
@@ -318,6 +318,10 @@
             var currentLength = maxCharacters - characters;
             document.getElementById('characterCount').textContent = currentLength + " characters remaining";
             });
+        
+        document.getElementById('submitComment').addEventListener('click', function() {
+            document.getElementById('characterCount').textContent = "5000 characters remaining";
+        });
     </script>
     <!-- BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
