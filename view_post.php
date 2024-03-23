cbb06c6828857ff117d96b6026ed6ee4ca434d2d
carsondrobe <carsondrobe@gmail.com>
2024-03-22 21:02:12 -0700

    back to top on view post

--- view_post.php
+++ view_post.php
@@ -273,6 +273,40 @@
         </div>
     </div>
 
+    <!-- Back to top button -->
+    <button type="button" class="btn btn-outline-black" id="btn-back-to-top">
+        Back to Top
+    </button>
+
+    <!-- Back to Top Logic -->
+    <script>
+        //Get the button
+        let mybutton = document.getElementById("btn-back-to-top");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction();
+        };
+
+        function scrollFunction() {
+            if (
+                document.body.scrollTop > 500 ||
+                document.documentElement.scrollTop > 500
+            ) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+        // When the user clicks on the button, scroll to the top of the document
+        mybutton.addEventListener("click", backToTop);
+
+        function backToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
+
 </body>
 
 </html>
