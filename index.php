a5dd3b6a039ae1fe6541b0d8b6203fedc6d76acd
carsondrobe <carsondrobe@gmail.com>
2024-03-22 22:16:48 -0700

    debug

--- index.php
+++ index.php
@@ -249,6 +249,7 @@
             xhr.open("GET", "search_users.php?query=" + searchQuery, true);
             xhr.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
                     document.getElementById('user-list').innerHTML = this.responseText;
                 }
             };
