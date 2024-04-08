0ccb862f410acc7bb139378eda509f02a844eb67
carsondrobe <carsondrobe@gmail.com>
2024-04-07 19:51:52 -0700

    make username read only

--- account.php
+++ account.php
@@ -1,5 +1,5 @@
-<?php 
-include 'navbar.php'; 
+<?php
+include 'navbar.php';
 include 'config.php';
 
 if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
@@ -48,18 +48,16 @@ mysqli_close($conn);
             <!-- Profile Picture Column -->
             <div class="col-md-4">
                 <button type="button" data-bs-toggle="modal" data-bs-target="#uploadProfilePicModal" style="background: none; border: none; padding: 0;">
-                <?php
-                
-                $profilePicPath = $row['profile_picture'];
-                if (empty($profilePicPath)) {
-                   
-                    $profilePicPath = "img/defaultprofile.jpeg";
-                } else {
-                    
-                    $profilePicPath = "handlepfp.php?user_id=" . urlencode($user_id);
-                }
-                ?>
-                <img src="<?php echo $profilePicPath; ?>" class="img-fluid rounded-circle" alt="Profile Picture">
+                    <?php
+
+                    $profilePicPath = $row['profile_picture'];
+                    if (empty($profilePicPath)) {
+                        $profilePicPath = "img/defaultprofile.jpeg";
+                    } else {
+                        $profilePicPath = "handlepfp.php?user_id=" . urlencode($user_id);
+                    }
+                    ?>
+                    <img src="<?php echo $profilePicPath; ?>" class="img-fluid rounded-circle" alt="Profile Picture">
                 </button>
             </div>
 
@@ -72,14 +70,14 @@ mysqli_close($conn);
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                        <form id="updateProfilePicForm" method="POST" action="updatepfp.php" enctype="multipart/form-data">
-                            <div class="mb-3">
-                                <label for="profilePicInput" class="form-label">Select New Profile Picture</label>
-                                <input type="file" class="form-control" id="profilePicInput" name="profilePicInput" accept="image/*">
-                                <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
-                            </div>
-                            <button type="submit" class="btn btn-primary">Upload</button>
-                        </form>
+                            <form id="updateProfilePicForm" method="POST" action="updatepfp.php" enctype="multipart/form-data">
+                                <div class="mb-3">
+                                    <label for="profilePicInput" class="form-label">Select New Profile Picture</label>
+                                    <input type="file" class="form-control" id="profilePicInput" name="profilePicInput" accept="image/*">
+                                    <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Upload</button>
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -87,59 +85,61 @@ mysqli_close($conn);
 
             <!-- Account Information Form Column -->
             <div class="col-md-8">
-            <h2>My Account</h2>
-            <form id="updateForm" method="POST" action="update_info.php">
-                <div class="mb-3">
-                    <label for="username" class="form-label">Username</label>
-                    <input type="text" class="form-control" id="username" name="username" value="<?php echo $row['username']; ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" name="email" value="<?php echo $row['email']; ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="DOB" class="form-label">Date of Birth</label>
-                    <input type="date" class="form-control" id="DOB" name="DOB" value="<?php echo $row['dob']; ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" value="<?php echo $row['password']; ?>">
-                </div>
-                <div class="mb-3">
-                    <label for="new_password" class="form-label">New Password (leave blank to keep the same)</label>
-                    <input type="password" class="form-control" id="new_password" name="new_password">
-                </div>
-                <button type="submit" class="btn btn-primary">Update Information</button>
-            </form>
+                <h2>My Account</h2>
+                <form id="updateForm" method="POST" action="update_info.php">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <span id="username" class="form-control">
+                            <?php echo htmlspecialchars($row['username']); ?>
+                        </span>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email" value="<?php echo $row['email']; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="DOB" class="form-label">Date of Birth</label>
+                        <input type="date" class="form-control" id="DOB" name="DOB" value="<?php echo $row['dob']; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" value="<?php echo $row['password']; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="new_password" class="form-label">New Password (leave blank to keep the same)</label>
+                        <input type="password" class="form-control" id="new_password" name="new_password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Update Information</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="container">
-    <div class="row justify-content-start">
-        <h3>My posts:</h3>
-        <ul class="list-group">
-            <?php
-            if ($posts_result) {
-                while ($post = mysqli_fetch_assoc($posts_result)) {
-                    $title = htmlspecialchars($post['title']);
-                    $post_id = htmlspecialchars($post['discussionID']);
-                    // Make sure to adjust the onclick function to properly handle the post_id
-                    echo "<li class='list-group-item d-flex justify-content-between align-items-center' onclick='window.location.href=\"view_post.php?post_id=$post_id\";' style='cursor: pointer;'>$title<button type='button' class='btn btn-danger' onclick='event.stopPropagation(); deletePost($post_id);'>Delete</button></li>";
+    <div class="container">
+        <div class="row justify-content-start">
+            <h3>My posts:</h3>
+            <ul class="list-group">
+                <?php
+                if ($posts_result) {
+                    while ($post = mysqli_fetch_assoc($posts_result)) {
+                        $title = htmlspecialchars($post['title']);
+                        $post_id = htmlspecialchars($post['discussionID']);
+                        // Make sure to adjust the onclick function to properly handle the post_id
+                        echo "<li class='list-group-item d-flex justify-content-between align-items-center' onclick='window.location.href=\"view_post.php?post_id=$post_id\";' style='cursor: pointer;'>$title<button type='button' class='btn btn-danger' onclick='event.stopPropagation(); deletePost($post_id);'>Delete</button></li>";
+                    }
+                } else {
+                    echo "Error fetching posts: " . mysqli_error($conn);
                 }
-            } else {
-                echo "Error fetching posts: " . mysqli_error($conn);
-            }
-            ?>
-        </ul>
+                ?>
+            </ul>
+        </div>
     </div>
-</div>
 
-<!-- BOOTSTRAP -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<!-- BOOTSTRAP -->
+    <!-- BOOTSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- BOOTSTRAP -->
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 </body>
 
