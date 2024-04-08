271598d2f7d9dc092f3e361a5d680e581fc892f2
carsondrobe <carsondrobe@gmail.com>
2024-04-07 19:13:03 -0700

    refactoring chart code

--- index.php
+++ index.php
@@ -73,18 +73,7 @@
 
     <!-- Script for usage chart generation and closing -->
     <script src="usage_charts.js"></script>
-    <style>
-        #chartContainer {
-            width: 60%;
-            margin: auto;
-            padding: 20px;
-        }
-
-        #categoryChart {
-            width: 100%;
-            height: auto;
-        }
-    </style>
+    <link href="css/chart_styling.css" rel="stylesheet">
 
     <!-- BOOTSTRAP -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
