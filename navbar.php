8adc6c4a8884ebe63bb88342dad9b6261625cc15
carsondrobe <carsondrobe@gmail.com>
2024-04-09 12:46:50 -0700

    fixingnavbar

--- navbar.php
+++ navbar.php
@@ -44,7 +44,7 @@ require 'config.php'; // Include your database connection configuration file
 
 
         <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
                 <li class="nav-item">
                     <a class="nav-link" href="about_us.php">About Us</a>
                 </li>
