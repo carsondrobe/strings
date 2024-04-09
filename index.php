8a146bbd93190ee732146960051e7a86e47e7306
carsondrobe <carsondrobe@gmail.com>
2024-04-09 13:13:23 -0700

    revert

--- index.php
+++ index.php
@@ -122,34 +122,31 @@
     </div>
 
     <!-- Back to top button -->
-    <button type="button" class="btn btn-outline-black d-none" id="btn-back-to-top">
+    <button type="button" class="btn btn-outline-black d-none d-md-block" id="btn-back-to-top">
         Back to Top
     </button>
 
     <!-- Back to Top Logic -->
     <script>
-        // Get the button
+        //Get the button
         let mybutton = document.getElementById("btn-back-to-top");
 
-        // Function to toggle button visibility on scroll
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction();
+        };
+
         function scrollFunction() {
-            if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-                $(mybutton).addClass("show-button");
+            if (
+                document.body.scrollTop > 500 ||
+                document.documentElement.scrollTop > 500
+            ) {
+                mybutton.style.display = "block";
             } else {
-                $(mybutton).removeClass("show-button");
+                mybutton.style.display = "none";
             }
         }
-
-        // Attach scroll event
-        window.onscroll = scrollFunction;
-
-        // Back to top function
-        function backToTop() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
-
-        // Attach click event
+        // When the user clicks on the button, scroll to the top of the document
         mybutton.addEventListener("click", backToTop);
 
         function backToTop() {
